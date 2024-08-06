09bf9b43152a965b791644c8451a31568008c113
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-08-06 01:55:16 +0000

    updated models.py to include star wars blog data

--- src/models.py
+++ src/models.py
@@ -1,19 +1,293 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    planets = db.relationship("Favorites", backref="user")
+    characters = db.relationship("Favorites", backref="user")
+    vehicles = db.relationship("Favorites", backref="user")
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
+    def __init__(self, user_name, email):
+        self.user_name = user_name
+        self.email = email
+        db.session.add(self)
+        try: 
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise Exception(error.args)
+    
     def serialize(self):
+        favorites_dictionaries = []
+        for favorite in self.planets:
+            favorites_dictionaries.append(
+                favorite.serialize()
+            )
+
         return {
             "id": self.id,
+            "user_name": self.user_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorites": favorites_dictionaries,
         }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
+
+    def __init__(self, user_id, planet_id, character_id, vehicle_id):
+        self.user_id = user_id
+        self.planet_id = planet_id
+        self.character_id = character_id
+        self.vehicle_id = vehicle_id
+        db.session.add(self)
+        db.session.commit()
+    
+    def serialize(self):
+        return {
+            **self.planet.serialize()
+            **self.character.serialize()
+            **self.vehicle.serialize()
+        }
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(1000), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    # favorites_id = db.Column(db.Integer, db.ForeignKey('favorites.id'))
+    # favorites = db.relationship(Favorites)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    favorites = db.relationship("Favorites", backref="planet")
+
+    def __init__(self, url, diameter, rotation_period, orbital_period, 
+                 name, terrain, population, gravity, climate, user_id):
+        self.url = url
+        self.diameter = diameter
+        self.rotation_period = rotation_period
+        self.orbital_period = orbital_period
+        self.name = name
+        self.terrain = terrain
+        self.population = population
+        self.gravity = gravity
+        self.climate = climate
+        self.user_id = user_id
+        db.session.add(self)
+        db.session.commit()
+    
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "url": self.url,
+            "diameter": self.diameter ,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "name": self.name, 
+            "terrain": self.terrain,
+            "population": self.population,
+            "gravity": self.gravity,
+            "climate": self.climate,
+            "user_id": self.user_id,
+        }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(1000), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    hair_color = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    mass = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    # favorites_id = db.Column(db.Integer, db.ForeignKey('favorites.id'))
+    # favorites = db.relationship(Favorites)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    favorites = db.relationship("Favorites", backref="character")
+
+    def __init__(self, url, name, hair_color, skin_color, 
+                 eye_color, birth_year, height, mass, gender, user_id):
+        self.url = url
+        self.name = name
+        self.hair_color = hair_color
+        self.skin_color = skin_color
+        self.eye_color = eye_color
+        self.birth_year = birth_year
+        self.height = height
+        self.mass = mass
+        self.gender = gender
+        self.user_id = user_id
+        db.session.add(self)
+        db.session.commit()
+    
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "url": self.url,
+            "name": self.name ,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color, 
+            "birth_year": self.birth_year,
+            "height": self.height,
+            "mass": self.mass,
+            "gender": self.gender,
+            "user_id": self.user_id,
+        }
+
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(1000), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    vehicle_class = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+    model = db.Column(db.String(250), nullable=False)
+    crew = db.Column(db.Integer, nullable=False)
+    cost_in_credits = db.Column(db.Integer, nullable=False)
+    length = db.Column(db.Integer, nullable=False)
+    passengers = db.Column(db.Integer, nullable=False)
+    max_atmosphering_speed = db.Column(db.Integer, nullable=False)
+    cargo_capacity = db.Column(db.Integer, nullable=False)
+    consumables = db.Column(db.String(250), nullable=False)
+    # favorites_id = db.Column(db.Integer, db.ForeignKey('favorites.id'))
+    # favorites = db.relationship(Favorites)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    favorites = db.relationship("Favorites", backref="vehicle")
+
+    def __init__(self, url, name, vehicle_class, manufacturer, 
+                 model, crew, cost_in_credits, length, passengers, 
+                 max_atmosphering_speed, cargo_capacity, consumables, 
+                 user_id):
+        self.url = url
+        self.name = name
+        self.vehicle_class = vehicle_class
+        self.manufacturer = manufacturer
+        self.model = model
+        self.crew = crew
+        self.cost_in_credits = cost_in_credits
+        self.length = length
+        self.passengers = passengers
+        self.max_atmosphering_speed = max_atmosphering_speed
+        self.cargo_capacity = cargo_capacity
+        self.consumables = consumables
+        self.user_id = user_id
+        db.session.add(self)
+        db.session.commit()
+    
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "name": self.name ,
+            "vehicle_class": self.vehicle_class,
+            "manufacturer": self.manufacturer,
+            "model": self.model,
+            "crew": self.crew,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "passengers": self.passengers,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "user_id": self.user_id,
+        }
+    
+    #################################################33
+
+# class Family(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     last_name = db.Column(db.String(40), nullable=False) 
+#     members = db.relationship("FamilyBond", backref="family")
+    
+#     def __init__(self, last_name):
+#         self.last_name = last_name
+#         db.session.add(self)
+#         try: 
+#             db.session.commit()
+#         except Exception as error:
+#             db.session.rollback()
+#             raise Exception(error.args)
+    
+#     def serialize(self):
+#         bond_dictionaries = []
+#         for bond in self.members:
+#             bond_dictionaries.append(
+#                 bond.serialize()
+#             )
+#         return {
+#             "id": self.id,
+#             "last_name": self.last_name,
+#             "members": bond_dictionaries
+#         }
+    
+# class Member(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     first_name = db.Column(db.String(40), nullable=False) 
+#     age = db.Column(db.Integer)
+#     led_family_id = db.Column(db.Integer, db.ForeignKey("family.id"))
+#     bonds = db.relationship("FamilyBond", backref="member")
+
+#     def __init__(self, first_name, age):
+#         self.first_name = first_name
+#         self.age = age
+#         self.led_family_id = None
+#         db.session.add(self)
+#         db.session.commit()
+    
+#     def serialize(self): 
+#         return {
+#             "id": self.id,
+#             "first_name": self.first_name,
+#             "age": self.age,
+#             "led_family_id": self.led_family_id
+#         }
+
+# class FamilyBond(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     date = db.Column(db.DateTime(timezone=True), default=func.now())
+#     member_id = db.Column(db.Integer, db.ForeignKey("member.id"))
+#     family_id = db.Column(db.Integer, db.ForeignKey("family.id"))
+
+#     def __init__(self, member_id, family_id):
+#         self.member_id = member_id
+#         self.family_id = family_id
+#         db.session.add(self)
+#         db.session.commit()
+    
+#     def serialize(self):
+#         return {
+#             **self.member.serialize()
+#         }
+
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
